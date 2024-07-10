fbc532bf74d7e9ee9e97908b08fc84b7e9d18883
ShimonBaruch <shimoba2@ac.sce.ac.il>
2024-07-10 18:36:43 +0300

    Initial commit

---
+++ src/connections/firebaseConfig.js
@@ -0,0 +1,20 @@
+// src/connections/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqzjwey29Aafm8PGWG3gimEGtFRoSM7WI",
+    authDomain: "ai-aid.firebaseapp.com",
+    projectId: "ai-aid",
+    storageBucket: "ai-aid.appspot.com",
+    messagingSenderId: "926150015973",
+    appId: "1:926150015973:web:05f8d08079076a76c28d0c",
+    measurementId: "G-QBC1X5JR64"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
