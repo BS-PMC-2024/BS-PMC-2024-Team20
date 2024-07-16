680c5dbeb8eca1fc4dbb6b76ebade1afedb8184c
AviadTu <aviadq@gmail.com>
2024-07-16 13:09:34 +0300

    BSPMS2420-16 update register modal for student

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../../styles/common.css';
 
 const StudentDashboard = () => {
   const [firstName, setFirstName] = useState('');
