584420ed9cc75a21ae884dc270cb775ce78c00d8
ShimonBaruch <shimoba2@ac.sce.ac.il>
2024-07-17 22:15:50 +0300

    Creating an admin home page BSPMS2420-10

---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+//Creating an admin home page BSPMS2420-10
+import React from 'react';
+
+const AdminHome = () => {
+  return (
+    <div>
+      <h1>Welcome, Admin</h1>
+      <p>This is your home page.</p>
+    </div>
+  );
+};
+
+export default AdminHome;
